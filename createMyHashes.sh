d4cbe655a6a4d765a6ca95df199ef1e2bc5eb00d
johnnyNcsu <johnny.hark@gmail.com>
2024-02-11 13:06:53 -0500

    Removed commented out code.

--- createMyHashes.sh
+++ createMyHashes.sh
@@ -6,24 +6,15 @@
 #    operation in the comparison stage and possible an auto-fetch operation.
 #
 
-#KEY_FILE_PATH='./'
 KEY_FILE_PREFIX='keyAppList'
-#KEY_FILE_DELIMITER='_'
-#UNIQUE_ID_REGEX='[[:alnum:]]{6}'
-#KEY_FILE_SUFFIX='.txt'
 
-#UPLOAD_FILE_PATH=$KEY_FILE_PATH
 UPLOAD_FILE_PREFIX='upload'
-#UPLOAD_FILE_DELIMITER=$KEY_FILE_DELIMITER
-#UPLOAD_FILE_SUFFIX=$KEY_FILE_SUFFIX
 
 usage() {
     >&2 printf 'Usage: ./%s file.csv\n' "${0##*/}"
     >&2 printf '   where file.csv is the name of the file exported from the Configurator tool.\n'
     exit 1
 }
-
-#UPLOAD_FILE="${UPLOAD_FILE_PATH}${UPLOAD_FILE_PREFIX}\\${UPLOAD_FILE_DELIMITER}${UNIQUE_ID_REGEX}\\${UPLOAD_FILE_SUFFIX}"
 
 if [ -f "$1" ] ; then
     case $1 in
